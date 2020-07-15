6e67321be068493d11cb7fbc75158d131d611337
Ken Sodemann <ken@ionic.io>
2020-07-15 08:18:07 -0500

    add a card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { pin } from 'ionicons/icons';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const cardClicked = () => {
+    console.log('the card was clicked');
+  };
+
+  const buttonClicked = (evt: any) => {
+    console.log('the button was clicked');
+    (evt as Event).stopPropagation();
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +38,19 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonCard button={true} onClick={cardClicked}>
+          <IonItem>
+            <IonIcon icon={pin} slot="start" />
+            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
+            <IonButton fill="outline" slot="end" onClick={buttonClicked}>
+              View
+            </IonButton>
+          </IonItem>
+
+          <IonCardContent>
+            This is content, without any paragraph or header tags, within an ion-cardContent element.
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
